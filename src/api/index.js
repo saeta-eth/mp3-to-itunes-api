34be0199e0f4d83914ce5553a270ffd5a86abfcc
slorenzo <SebastianLorenzo@gmail.com>
2018-01-15 19:56:17 -0300

    Added /upload endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,13 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
+import upload from './upload';
+import converter from './converter';
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	api.use('/upload', upload({ config, db }));
+  //api.use('/converter', converter({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
