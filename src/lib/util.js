34ec8917ca3e97c6a887ed3e08691e0979d0d82d
slorenzo <SebastianLorenzo@gmail.com>
2018-01-22 23:54:03 -0300

    Added checkAverageExtension, removeFilesExceptExt, checkEmptyFolder, getFilesFromFolder, and checkFolders methods

--- src/lib/util.js
+++ src/lib/util.js
@@ -38,31 +38,37 @@ export function decompressedFile(pathCompressedFile, pathDeCompressedFile, exten
 }
 
 export async function checkAverageExtension(extensions, path) {
-  const files = await fs.readdir(path);
-  let filesAccepted = [];
+  let files = await fs.readdir(path);
   if(files.length) {
-    for (let file of files) {
-      for (let extension of extensions) {
-        if(file.indexOf(extension) !== -1) {
+    if(files.length === 1 || (files.length === 2 && files.indexOf('__MACOSX') !== -1)) {
+      files = await getFilesFromFolder(`${path}/${files[0]}`);
+    }
+
+    let filesAccepted = [];
+    for (let extension of extensions) {
+      for (let file of files) {
+        if(file.includes(extension)) {
           filesAccepted.push(file);
-        }
-      } 
+        } 
+      }
     }
 
     return filesAccepted.length/files.length;
   }
-
-  return 0;
+  return 0
 }
 
 export async function removeFilesExceptExt(extensions, path) {
-  const files = await fs.readdir(path);
-  for (let file of files) {
-    for (let extension of extensions) {
-      if(file.indexOf(extension) === -1) {
-        await fs.delete(`${path}/${file}`);
-      } 
+  try{
+    const { files, newPath } = await checkFolders(path);
+    for (let file of files) {
+      const fileExtension = file.split('.').pop();
+      if(!extensions.includes(fileExtension)){
+        await fs.delete(`${newPath}/${file}`);
+      }
     }
+  } catch(err) {
+    throw new Error(err);
   }
 }
 
@@ -76,4 +82,31 @@ export async function getFilesFromFolder(path) {
   return files.map((file) => {
     return `${path}/${file}`;
   });
+}
+
+export async function checkFolders(path) {
+  let files = await fs.readdir(path);
+  if(files.length >= 3){
+    throw new Error('The folder or file has deeper directories');
+  }
+  if(files.length) {
+    if(files.length === 1 || files.length === 2) {
+      if(files.indexOf('__MACOSX') !== -1) {
+        await fs.delete(`${path}/__MACOSX`);  
+      }
+      const newPath = `${path}/${files[0]}`;
+      files = await fs.readdir(newPath);
+
+      return {
+        files,
+        newPath
+      }
+    }
+    return {
+      files,
+      [newPath] : path
+    };
+  } else {
+    throw new Error('The folder or file does not have files');
+  }
 }
