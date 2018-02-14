d2f93c4559a27b4e567187e98343dd55109c8d69
slorenzo <SebastianLorenzo@gmail.com>
2018-02-14 19:02:42 -0300

    Added new way to descompress file

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,37 +1,10 @@
-import unzipper from 'unzipper';
-//import tar from 'tar-fs';
+import zip from 'file-zip';
 import * as fs from 'async-file';
 import * as fsx from 'fs';
 
-/**	Creates a callback that proxies node callback style arguments to an Express Response object.
- *	@param {express.Response} res	Express HTTP Response
- *	@param {number} [status=200]	Status code to send on success
- *
- *	@example
- *		list(req, res) {
- *			collection.find({}, toRes(res));
- *		}
- */
-export function toRes(res, status = 200) {
-	return (err, thing) => {
-		if (err) return res.status(500).send(err);
-
-		if (thing && typeof thing.toObject === 'function') {
-			thing = thing.toObject();
-		}
-		res.status(status).json(thing);
-	};
-}
-
 export function decompressedFile(pathCompressedFile, pathDeCompressedFile, extension, cb) {
   if (extension === '.zip') {
-    fsx.createReadStream(pathCompressedFile).pipe(unzipper.Extract({ path: pathDeCompressedFile }))
-      .on('close', () => {
-        cb();
-      })
-      .on('error', (err) => {
-        cb(err);
-      });
+    zip.unzip(pathCompressedFile, pathDeCompressedFile, cb);
   } else {
     // Another extension.
   }
