603fc3f66854235d32a735e3a86d46a879491f1f
slorenzo <SebastianLorenzo@gmail.com>
2018-02-14 19:03:30 -0300

    Added folder exist validation

--- src/api/upload.js
+++ src/api/upload.js
@@ -42,15 +42,21 @@ export default ({ config, db }) => resource({
 		      return res.status(500).send(err);
 		    }
 		    
-		    decompressedFile(pathCompressedFile, pathDeCompressedFile, path.extname(req.files.compressedFile.name), (err) => {
+		    decompressedFile(pathCompressedFile, pathDeCompressedFile, path.extname(req.files.compressedFile.name), async (err) => {
 		    	if (err) {
 			      return res.status(500).send(err);
 			    }
-			    return res.status(200).json({
-			    	fileName: newName
-			    });
+          try{
+            const url = `http://${config.URL_DEV}:${config.PORT_DEV}/api/upload/${newName}`;
+            await axios.put(url);
+            return res.status(200).json({
+              fileName: newName
+            });
+          } catch(err) {
+            logger.error(err);
+            return res.status(500).send(err);
+          }
 		    });
-
 		  });
 		} else {
 			return res.status(500).send('The file is not a zip or tar.');
@@ -64,45 +70,51 @@ export default ({ config, db }) => resource({
     
     const folderName = req.params.upload;
     const pathDeCompressedFile = `${process.env.PWD}/decompressed/${folderName}`;
+    if (!(await fs.exists(pathDeCompressedFile))) {
+      return res.status(500).json({
+        status: 404,
+        message: `The ID: ${folderName} is not found.`
+      });
+    }
     const percentageAcceptedExtensions = await checkAverageExtension(config.EXTENSION_ACCEPTED, pathDeCompressedFile)
     if (percentageAcceptedExtensions > config.PERCENTAGE_ACCEPTED) {
-    	// @slorenzo: If is an album, I remove other files with different extension.
-    	try {
-    		removeFilesExceptExt(config.EXTENSION_ACCEPTED, pathDeCompressedFile);
-    		return res.status(200).json({
-	    		message: 'The .zip was successfully loaded'
-	    	});
-    	} catch(err) {
-    		logger.error(err);
-    		return res.status(500).send(err);
-    	}
+      // @slorenzo: If is an album, I remove other files with different extension.
+      try {
+        removeFilesExceptExt(config.EXTENSION_ACCEPTED, pathDeCompressedFile);
+        return res.status(200).json({
+          message: 'The .zip was successfully loaded'
+        });
+      } catch(err) {
+        logger.error(err);
+        return res.status(500).send(err);
+      }
     } else {
-    	const url = `http://${config.URL_DEV}:${config.PORT_DEV}/api/upload/${req.params.upload}`;
-    	try{
-    		await axios.delete(url);
-    		return res.status(500).json({
-	    		status: 500,
-	    		message: 'The .zip not contains .mp3 files'
-	    	});
-    	} catch(err) {
-    		logger.error(err);
-    		return res.status(500).send(err);
-    	}
+      const url = `http://${config.URL_DEV}:${config.PORT_DEV}/api/upload/${req.params.upload}`;
+      try{
+        await axios.delete(url);
+        return res.status(500).json({
+          status: 500,
+          message: 'The .zip not contains .mp3 files'
+        });
+      } catch(err) {
+        logger.error(err);
+        return res.status(500).send(err);
+      }
     }
-	},
+  },
 
-	/** DELETE /:id - Delete a given entity */
-	async delete(req, res) {
-		const id = req.params.upload;
-		try {
-			await fs.unlink(`${process.env.PWD}/compressed/${id}.zip`);
-			await fs.rimraf(`${process.env.PWD}/decompressed/${id}`);
-			return res.status(200).json({
-    		message: `All about ${id} were removed`
-    	});
-		} catch(err) {
-			logger.error(err);
-			return res.status(500).json(err);
-		}
-	}
+  /** DELETE /:id - Delete a given entity */
+  async delete(req, res) {
+    const id = req.params.upload;
+    try {
+      await fs.unlink(`${process.env.PWD}/compressed/${id}.zip`);
+      await fs.rimraf(`${process.env.PWD}/decompressed/${id}`);
+      return res.status(200).json({
+        message: `All about ${id} were removed`
+      });
+    } catch(err) {
+      logger.error(err);
+      return res.status(500).json(err);
+    }
+  }
 });
